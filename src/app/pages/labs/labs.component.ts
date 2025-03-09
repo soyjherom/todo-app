a315dd2429f66417e1b92f65dda29e3adb2623fc
soyjherom <jherom@tecnica.co.cr>
2025-03-08 18:31:17 -0600

    Property binding

--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -16,5 +16,10 @@ export class LabsComponent {
     'Crear el proyecto',
     'Crear componentes'
   ];
-  dev_name = 'Jherom';
+  disabled = true;
+  developer = {
+    name : 'Jherom',
+    age : 38,
+    photo_url : 'https://media.licdn.com/dms/image/v2/D4E03AQF8y85Ba9jrtA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1732926507451?e=1747267200&v=beta&t=lIfkBPNoZgy_DXBxYzkdr6t0O-FnNK9HwNo8h7Lvh10'
+  }
 }
